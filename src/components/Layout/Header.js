9735a2c987674f3fb4f93409bf91c627b2b876eb
mgsrevolver <clayreimus@gmail.com>
2022-05-30 14:10:38 -0400

    added cards with styling and a MealItem component

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,18 +1,20 @@
-import React from 'react'
+import { Fragment } from 'react'
+
+import HeaderCartButton from './HeaderCartButton'
 import mealsImage from '../../assets/meals.jpeg'
 import classes from './Header.module.css'
 
 const Header = (props) => {
   return (
-    <React.Fragment>
+    <Fragment>
       <header className={classes.header}>
         <h1>ReactMeals</h1>
-        <button>Cart</button>
+        <HeaderCartButton />
       </header>
       <div className={classes['main-image']}>
-        <img src={mealsImage} alt="Table full of food" />
+        <img src={mealsImage} alt="A table full of delicious food!" />
       </div>
-    </React.Fragment>
+    </Fragment>
   )
 }
 
