b147bf2c917eb98a868c58b5876fa67a8cb765ca
mgsrevolver <clayreimus@gmail.com>
2022-05-30 14:29:09 -0400

    did half of the form and input components

---
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -0,0 +1,26 @@
+import { Fragment } from 'react'
+import classes from './MealItemForm.module.css'
+import Input from '../../UI/Input/Input'
+
+const MealItemForm = (props) => {
+  return (
+    <Fragment>
+      <form className={classes.form}>
+        <Input
+          label="Amount"
+          input={{
+            id: 'amount_' + props.id,
+            type: 'number',
+            min: '1',
+            max: '5',
+            step: '1',
+            defaultValue: '1',
+          }}
+        />
+        <button className={classes.button}>+ Add</button>
+      </form>
+    </Fragment>
+  )
+}
+
+export default MealItemForm
---
+++ src/components/UI/Card/Input/Input.js
@@ -0,0 +1,7 @@
+import classes from './Input.module.css'
+
+const Input = () => {
+  return <input className={classes.input}></input>
+}
+
+export default Input
