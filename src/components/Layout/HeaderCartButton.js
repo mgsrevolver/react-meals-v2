9d01034ab8d72f1b8c8123e235834fab8d59a90d
mgsrevolver <clayreimus@gmail.com>
2022-06-02 14:36:54 -0400

    cheated to add cart items

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,4 +1,5 @@
-import { React, useContext } from 'react'
+import { useContext } from 'react'
+
 import CartIcon from '../Cart/CartIcon'
 import CartContext from '../../store/cart-context'
 import classes from './HeaderCartButton.module.css'
--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -1,16 +1,17 @@
 import { useReducer } from 'react'
+
 import CartContext from './cart-context'
 
 const defaultCartState = {
   items: [],
-  totalAmout: 0,
+  totalAmount: 0,
 }
 
 const cartReducer = (state, action) => {
   if (action.type === 'ADD') {
     const updatedItems = state.items.concat(action.item)
-    const updatedTotalAmount = (state.totalAmount =
-      action.item.price * action.item.amount)
+    const updatedTotalAmount =
+      state.totalAmount + action.item.price * action.item.amount
     return {
       items: updatedItems,
       totalAmount: updatedTotalAmount,
